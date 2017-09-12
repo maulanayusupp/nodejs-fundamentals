6e1816b14fa5f70c9fd3bc2cce29621ba65f615b
Maulana Yusup Abdullah <maulanayusupp@gmail.com>
2017-09-12 11:31:05 +0700

    NPM Works

--- notes-node/app.js
+++ notes-node/app.js
@@ -8,8 +8,13 @@ const os = require('os')
 
 /* notes js */
 const notes = require('./notes.js');
-var result = notes.addNote();
 
-console.log('Result: ', notes.add(9,-2));
+
+/* append file */
 /*var user = os.userInfo();
 fs.appendFile('greetings.txt', `Bro ${user.username}! Your are ${notes.age}`);*/
+
+/* lodash */
+const _ = require('lodash');
+var filteredArray = _.uniq(['Maulana', 1, 'Yusup', 1, 2, 3, 4, 4, 5]);
+console.log(filteredArray)
