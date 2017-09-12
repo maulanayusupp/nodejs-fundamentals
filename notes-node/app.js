ec310a41f760db6f38419a036777578eba896885
Maulana Yusup Abdullah <maulanayusupp@gmail.com>
2017-09-12 11:40:57 +0700

    Automatically Restart Node Application with Nodemon

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,5 +1,7 @@
 console.log("Starting App.");
 
+/* nodemon - simple monitoring and automatically restart node application */
+
 /* file system */
 const fs = require('fs');
 
@@ -9,12 +11,12 @@ const os = require('os')
 /* notes js */
 const notes = require('./notes.js');
 
+/* lodash */
+const _ = require('lodash');
 
 /* append file */
 /*var user = os.userInfo();
 fs.appendFile('greetings.txt', `Bro ${user.username}! Your are ${notes.age}`);*/
 
-/* lodash */
-const _ = require('lodash');
-var filteredArray = _.uniq(['Maulana', 1, 'Yusup', 1, 2, 3, 4, 4, 5]);
+var filteredArray = _.uniq(['Yusup', 1, 'Maulana', 1, 2, 3, 4, 4, 5]);
 console.log(filteredArray)
