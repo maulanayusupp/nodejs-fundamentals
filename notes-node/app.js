be8b6c1d19c936f540e219a3a8ca2e56449516cc
Maulana Yusup Abdullah <maulanayusupp@gmail.com>
2017-09-12 11:02:58 +0700

    Require another file

--- notes-node/app.js
+++ notes-node/app.js
@@ -6,5 +6,8 @@ const fs = require('fs');
 /* OS */
 const os = require('os')
 
+/* notes js */
+const notes = require('./notes.js');
+
 var user = os.userInfo();
 fs.appendFile('greetings.txt', `Bro ${user.username}!`);
